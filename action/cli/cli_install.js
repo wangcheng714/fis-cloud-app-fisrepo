bb5a865c1a7495a840c6dee8b5eace2b98c76a45
wangcheng <wangcheng714@gmail.com>
2013-09-18 14:02:14 +0800

    1. 添加markdown支持 2. 统计Component下载次数

--- action/cli/cli_install.js
+++ action/cli/cli_install.js
@@ -5,7 +5,7 @@ module.exports = function(req, res, app){
     Component.getComponentAttachment(req_component.name, req_component.version, function(error, file, type){
         if(!error){
             fis.db.read(file, {}, function(error, content){
-                Component.addDownloadTime(req_component, function(error){
+                Component.addTotaldowns(req_component.name, function(error){
                     if(!error){
                         res.set("filename", file);
                         res.set("Content-Type", type);
